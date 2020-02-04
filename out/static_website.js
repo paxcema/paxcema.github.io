45b7db1822281bcaf912fd77f3ad3c8b413628f4
Patricio Cerda Mardini <pcerdam@uc.cl>
2020-02-04 17:39:57 -0300

    Cleanup

--- out/static_website.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.10.520 {:static-fns true, :optimize-constants true}
-goog.provide('static_website');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-console.log("Hey there!");
