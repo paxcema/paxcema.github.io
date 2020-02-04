1bc25b02aa793eb46ff6c8e843ade8b62c6b6f8d
Patricio Cerda Mardini <pcerdam@uc.cl>
2020-02-03 22:02:50 -0300

    Added hiccup dep

---
+++ out/static_website.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 1.10.520 {:static-fns true, :optimize-constants true}
+goog.provide('static_website');
+goog.require('cljs.core');
+goog.require('cljs.core.constants');
+console.log("Hey there!");
--- src/website/static_website.cljs
+++ src/website/static_website.cljs
@@ -1,13 +1,16 @@
 ;;; Following: https://betweentwoparens.com/deoploy-clojurescript-to-github-pages#fn-project-name
 
 ;; To build dev bundle (from deps.edn folder):
-;; clj -m cljs.main -d "pcerdam.github.io/out" -c static-website -r
+;; clj -m cljs.main -d "pcerdam.github.io/out" -c website.static-website -r
+
+;; To build production bundle
+;; clj -m cljs.main -O advanced -c "website.static-website"
 
 ;; Project structure
 ;; deps.edn; index.html; style.css; README.md
 ;; - src/*.cljs (this file)
 ;; - pcerdam.github.io/out/*.files
 
-(ns static-website)
+(ns website.static-website)
 
 (js/console.log "Hey there!")
